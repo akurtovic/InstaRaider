ee749465d353cf0420669d175a334f0c95959b73
Amir Kurtovic <amirkurtovic@gmail.com>
2014-04-04 00:31:19 -0500

    First functional version to save photos

--- Raid.py
+++ Raid.py
@@ -1,29 +1,31 @@
 """
 Raid.py
-CL tool using instaRaised.py to archive public Instagram photo for a specified user
+CL tool using instaRaider.py to archive public Instagram photo for a specified user
 @amirkurtovic
-
-TODO: 
 """
-from instaRaider.py import *
+from instaRaider import *
 import sys
 
 if (len(sys.argv) != 2):
-	print "Useage: ./instaRaider.py username"
+	print "Useage: ./Raid.py username"
+else:
+        userName = str(sys.argv[1])
+        
+        # compile URL from argument
+        url = 'http://instagram.com/' + userName + '/'
+        
+        # check if Instagram username is valid
+        req = urllib2.Request(url)
 
-# compile URL from argument
-url = 'http://instagram.com/' + str(sys.argv[1]) + '/'
+        validUser = True
+        try:
+            urllib2.urlopen(req)
+        except:
+            print "Could not find the Instagram user"
+            validUser = False
 
-# check if Instagram username is valid
-req = urllib2.Request(url)
-try:
-    urllib2.urlopen(req)
-except URLError, e:
-    print e.code
-    print e.read()
-
-# Get source code from fully loaded Instagram profile page
-source = loadInstagram(url)
-
-# Download all photos identified on profile page
-getPhotos(source)
+        if(validUser):
+            # Get source code from fully loaded Instagram profile page
+            source = loadInstagram(url)
+            # Download all photos identified on profile page
+            getPhotos(source, userName)
--- instaRaider.py
+++ instaRaider.py
@@ -10,7 +10,13 @@ from bs4 import BeautifulSoup
 import selenium.webdriver as webdriver
 import re
 from time import sleep
+import urllib
 import urllib2
+import os
+import sys
+
+PAUSE = 1
+
 
 def getImageCount(userName):
     '''
@@ -20,7 +26,7 @@ def getImageCount(userName):
     '''
     response = urllib2.urlopen(url)
 
-    #fix regular expression to match correct tag
+    # fix regular expression to match correct tag
     imageCount = re.search(r'counts\":{\"media\":[^\s<>"]\,' , str(response.read()))
 
 def loadInstagram(url):
@@ -30,31 +36,51 @@ def loadInstagram(url):
     '''
     driver = webdriver.Firefox()
     driver.get(url)
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(PAUSE)
 
     # TODO: Once getImageCount functional, loop to load all images
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(3)
+    sleep(5)
     
     source = BeautifulSoup(driver.page_source)
     return source
 
-def getPhotos(source):
+def getPhotos(source, userName):
     '''
     Given source code for loaded Instagram page,
     extract all hrefs and download full-resolution photos
     
     source: HTML source code of Instagram profile papge
     '''
+    # directory where photos will be saved
+    directory = './Images/' + userName + '/'
+
+    # check if directory exists, if not, make it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    photonumber = 0
+    print "Raiding Instagram..."
+    print "Saving photos to " + directory
+    print "------"
     
     for x in source.findAll('li', {'class':'photo'}):
+
+        # increment photonumber for next image
+        photonumber += 1
 
         #extract url to AWS thumbnail from each photo
         rawUrl = re.search(r'url\(\"https?://[^\s<>"]+|www\.[^\s<>"]+', str(x))
         
         #format thumbnail url to lead to full-resolution photo
         photoUrl = str(rawUrl.group())[5:-5] + '8.jpg'
+        photoname = directory + str(photonumber) + '.jpg'
 
-        # print URL of full-resolution photo
-        # TODO: Use urllib2 to download photo to folder
-        print photoUrl
+        # save full-resolution photo
+        urllib.urlretrieve(photoUrl, photoname)
+        sys.stdout.write('#')
+        sys.stdout.flush()
+        sleep(PAUSE)
+
+    print "\n------"
+    print "Saved " + str(photonumber) + " images to " + directory
